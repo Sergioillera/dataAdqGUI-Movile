51ee04d9685a8122c3a613f4c617376ffe88d83d
Sergioillera <35736911+Sergioillera@users.noreply.github.com>
2019-05-03 19:30:03 +0200

    Update main_ui.py
    Added new info labels and pause button.
    Recoded the start condition for the threads

--- main_ui.py
+++ main_ui.py
@@ -34,26 +34,45 @@ class MainWindow(QtWidgets.QWidget): #creamos la ventana principal
         boton_init.move(self.ancho/2-70,self.alto-120)
         boton_init.resize(140,100)
         boton_init.clicked.connect(self.iniciar)
+        
+        #el boton_pause
+        boton_pause =QPushButton('Pause',self)
+        boton_pause.move(self.ancho/2+100,self.alto-90)
+        boton_pause.resize(50,50)
+        boton_pause.clicked.connect(self.pause)
 
         #el boton_exit
         boton_exit = QPushButton('Salir', self)
         boton_exit.move(self.ancho-70,self.alto-70)
         boton_exit.resize(50,50)
         boton_exit.clicked.connect(self.salir)
-    
-    
+        
+        #text box status + label
+        status_text = QLabel('Estado: ',self)
+        status_text.move(20, self.alto-100)
+        self.status_label = QLabel('Sin conexion        ',self)
+        self.status_label.move(70, self.alto-100)
+                
+        #text box medidas + label
+        measure_text = QLabel('Num. medidas: ',self)
+        measure_text.move(20, self.alto-80)
+        self.measure_label = QLabel('       ',self)
+        self.measure_label.move(100, self.alto-80)
+        
+                
     def iniciar(self):
         archivo='ej_datos.txt'
-        start=True
+        self.start=True
         q = queue.Queue()
-        thread1 = threading.Thread(target=self.read_data,args=(archivo,q))
-        thread2 = threading.Thread(target=self.recibir_and_plot,args=(start,q))
+        self.thread1 = threading.Thread(target=self.read_data,args=(archivo,q))
+        self.thread2 = threading.Thread(target=self.recibir_and_plot,args=(q,))
         print ('Iniciamos los threads')
-        thread1.start()
-        thread2.start()    
+        self.thread1.start()
+        self.thread2.start()    
     
     
     def read_data(self,arch,q):
+        self.status_label.setText('Leyendo datos...')
         archivo=open(arch,'r')
         dT=archivo.readline()
         dt=float(dT[dT.index('=')+1:-1])
@@ -68,14 +87,16 @@ class MainWindow(QtWidgets.QWidget): #creamos la ventana principal
         return
 
 
-    def recibir_and_plot(self,start,q):
+    def recibir_and_plot(self,q):
         dt=q.get() #hasta que reciba el primer dato
         print('El dt',dt)
         i=0
-        while start:
+        while self.start:
             dato=q.get() #las acceleraciones
-            print(i,dato)
+            #print(i,dato)
+            self.status_label.setText('Leyendo datos...')
             if dato==None:
+                self.status_label.setText('Datos leidos')
                 break
             else:
                 self.figura_plots.ax1.cla()
@@ -93,12 +114,21 @@ class MainWindow(QtWidgets.QWidget): #creamos la ventana principal
 
                 self.figura_plots.draw()
                 i+=1
+                if i%10==0:
+                    print(i)
+                    self.measure_label.setText(str(i))
         return         
     
     
     def salir(self):
+        self.start=False #in order to kill the recibir_and_plot thread
         self.close()
         return 
+    
+    def pause(self):
+        self.start=False #in order to kill the recibir_and_plot thread
+        self.status_label.setText('Pausado')
+        return
 
 
 class PlotCanvas(FigureCanvas):
@@ -165,77 +195,3 @@ if __name__ == "__main__":
 
 
 
-
-
-
-
-
-
-
-'''
-import sys
-
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, QPushButton
-from PyQt5.QtGui import QIcon
-
-
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
-
-import random
-
-class App(QMainWindow):
-
-    def __init__(self):
-        super().__init__()
-        self.left = 10
-        self.top = 10
-        self.title = 'PyQt5 matplotlib example - pythonspot.com'
-        self.width = 640
-        self.height = 400
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-
-        m = PlotCanvas(self, width=5, height=4)
-        m.move(0,0)
-
-        button = QPushButton('PyQt5 button', self)
-        button.setToolTip('This s an example button')
-        button.move(500,0)
-        button.resize(140,100)
-
-        self.show()
-
-
-class PlotCanvas(FigureCanvas):
-
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
-
-        FigureCanvas.setSizePolicy(self,
-                QSizePolicy.Expanding,
-                QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
-        #self.plot()
-
-
-    def plot(self):
-        data = [random.random() for i in range(25)]
-        ax = self.figure.add_subplot(111)
-        ax.plot(data, 'r-')
-        ax.set_title('PyQt Matplotlib Example')
-        self.draw()
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
-    sys.exit(app.exec_())
-'''
